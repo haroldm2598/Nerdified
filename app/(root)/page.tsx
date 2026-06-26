6bfc6252ae771f98e918684fbcdb330826efb51f
rold dev <cvuyante24@gmail.com>
2026-06-26 20:53:35 +0800

    to be deleted

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,18 +1,14 @@
 import BookCard from "@/components/BookCard";
 import HeroSection from "@/components/HeroSection";
 import { sampleBooks } from "@/lib/constants";
-<<<<<<< HEAD
 import { createClient } from "@/utils/supabase/server";
 import { cookies } from "next/headers";
 
 export default async function Home() {
     const cookieStore = await cookies();
     const supabase = createClient(cookieStore);
-    const { data: post } = await supabase.from("post").select();
-=======
->>>>>>> parent of 1f8fc1a (feat: supabase added)
+    const { data: todos } = await supabase.from("todos").select();
 
-export default function Home() {
     return (
         <section className="min-h-screen bg-[var(--bg-primary)] py-10">
             <div className="wrapper space-y-16">
@@ -29,22 +25,19 @@ export default function Home() {
                         />
                     ))}
                 </div>
-<<<<<<< HEAD
 
                 <div className="mt-8 rounded-lg border border-white/10 bg-white/5 p-4">
                     <h2 className="mb-3 text-lg font-semibold">
-                        Supabase post
+                        Supabase todos
                     </h2>
                     <ul className="space-y-2">
-                        {post?.map((todo) => (
+                        {todos?.map((todo) => (
                             <li key={todo.id} className="text-sm text-gray-300">
-                                {todo.title}
+                                {todo.name}
                             </li>
                         ))}
                     </ul>
                 </div>
-=======
->>>>>>> parent of 1f8fc1a (feat: supabase added)
             </div>
         </section>
     );
