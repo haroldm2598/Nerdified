6bce4fc921b150f9d5dd4ab54f17a7edee2b58f3
rold dev <cvuyante24@gmail.com>
2026-07-10 21:25:41 +0800

    feat: backend features for upload

--- lib/repositories/upload.repository.ts
+++ lib/repositories/upload.repository.ts
@@ -1,1 +1,15 @@
+import { prisma } from "../db";
+import type { Prisma } from "../generated/prisma/client";
+
 // dine ka gagawa nung prisma call then bato mo sa API Route handler to
+export function findAll() {
+    return prisma.book.findMany({
+        orderBy: {
+            createdAt: "desc",
+        },
+    });
+}
+
+export function create(data: Prisma.BookCreateInput) {
+    return prisma.book.create({ data });
+}
