f26c65fcd74e76539398448c95162c2e9d46b4a5
haroldm2598 <cvuyante24@gmail.com>
2026-07-22 15:28:57 +0800

    update: book.serivce.ts

--- lib/repositories/book.repository.ts
+++ lib/repositories/book.repository.ts
@@ -3,13 +3,28 @@ import type { Prisma } from "../generated/prisma/client";
 
 // dine ka gagawa nung prisma call then bato mo sa API Route handler to
 export function findAll() {
-    return prisma.book.findMany({
-        orderBy: {
-            createdAt: "desc",
-        },
-    });
+  return prisma.book.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
 }
 
 export function create(data: Prisma.BookCreateInput) {
-    return prisma.book.create({ data });
+  return prisma.book.create({ data });
 }
+
+// for duplication checking
+export function findByTitle(title: string) {
+  return prisma.book.findFirst({
+    where: { title },
+  });
+}
+
+// export function slugify(title: string): string {
+//   return title
+//     .toLowerCase()
+//     .trim()
+//     .replace(/[^a-z0-9]+/g, "-")
+//     .replace(/(^-|-$)/g, "");
+// }
--- lib/service/book.service.ts
+++ lib/service/book.service.ts
@@ -1,14 +1,19 @@
 import { CreateBook } from "@/types";
 import * as repository from "../repositories/book.repository";
+import { generateSlug } from "../utils";
 
 export async function getBooks() {
-    return repository.findAll();
+  return repository.findAll();
 }
 
 export async function createBook(data: CreateBook) {
-    // gawan ko muna ng repository tong function findByTitle
-    // const existingBook = await repository.findByTitle(data.title);
-    // if (existingBook) {
-    //     throw new Error("A book with this title already exists.");
-    // }
+  const existingBook = await repository.findByTitle(data.title);
+
+  if (existingBook) {
+    throw new Error("A book with this title already exists.");
+  }
+
+  const slug = generateSlug();
+
+  // return repository.create({...data, slug, coverURL: data.coverURL ?? ""})
 }
