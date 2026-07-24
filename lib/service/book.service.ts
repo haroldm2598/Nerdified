fbd9850c5da4f86ea8a8c7f94684afef5c284ba0
rold dev <cvuyante24@gmail.com>
2026-07-24 21:43:41 +0800

    bug: still a bug in api/book/route.ts

--- lib/service/book.service.ts
+++ lib/service/book.service.ts
@@ -1,19 +1,19 @@
-import { CreateBook } from "@/types";
+import { IBook } from "@/types";
 import * as repository from "../repositories/book.repository";
 import { generateSlug } from "../utils";
 
 export async function getBooks() {
-  return repository.findAll();
+    return repository.findAll();
 }
 
-export async function createBook(data: CreateBook) {
-  const existingBook = await repository.findByTitle(data.title);
+export async function createBook(data: IBook) {
+    const existingBook = await repository.findByTitle(data.title);
 
-  if (existingBook) {
-    throw new Error("A book with this title already exists.");
-  }
+    if (existingBook) {
+        throw new Error("A book with this title already exists.");
+    }
 
-  const slug = generateSlug();
+    const slug = generateSlug(data.title);
 
-  // return repository.create({...data, slug, coverURL: data.coverURL ?? ""})
+    return repository.create({ ...data, slug, totalSegments: 0 });
 }
